cb96ac439a3ab37057459db7eaffbd984ab80395
amirhoosian <amirhoosian78@gmail.com~>
2025-08-12 20:20:27 +0330

    feat(landing-footer): implement responsive multi-layer animated SVG waves for footer

--- src/app/components/LandingFooter/index.tsx
+++ src/app/components/LandingFooter/index.tsx
@@ -1,11 +1,25 @@
 export default function LandingFooter() {
   return (
-    <footer className="w-full mt-12 bg-base-300 border-t border-base-300 shadow-inner relative overflow-hidden text-white">
-      <img
-        src="/view/landingfooter_img.png"
-        alt="footer"
-        className="pointer-events-none absolute inset-x-0 top-0 w-full h-20 md:h-24 lg:h-28  object-top z-0"
-      />
+    <footer className="w-full  mt-12 bg-base-300 border-t border-base-300 shadow-inner relative  text-white">
+      <div className="wave-container relative w-full h-28 bg-[#0099ff] ">
+        <div
+          id="wave4"
+          className="absolute inset-x-0 -top-10 bottom-0 h-28 wave-layer wave-sparse animate-wave-14-2000 opacity-30 z-[1] origin-bottom scale-y-[1.5]"
+        />
+        <div
+          id="wave3"
+          className="absolute inset-x-0 -top-10 bottom-0 h-28 wave-layer wave-std    animate-wave-12-720  opacity-45 z-[2] origin-bottom scale-y-[1.6] delay--2s"
+        />
+        <div
+          id="wave2"
+          className="absolute inset-x-0 -top-10 bottom-0 h-28 wave-layer wave-std    animate-wave-10-1440 opacity-60 z-[3] origin-bottom scale-y-[1.7] delay--4s"
+        />
+        <div
+          id="wave1"
+          className="absolute inset-x-0 -top-10 bottom-0 h-28 wave-layer wave-dense  animate-wave-8-1440  opacity-80 z-[4] origin-bottom scale-y-[1.8] delay--6s"
+        />
+      </div>
+
       <div className="relative z-10 w-full">
         <div className="max-w-6xl mx-auto px-6 py-8 flex flex-col md:flex-row items-center md:justify-between gap-6">
           <p className="text-2xl font-extrabold text-primary">Phoenix Task</p>
