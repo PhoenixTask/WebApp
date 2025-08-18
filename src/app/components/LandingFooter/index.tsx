51f2cfc2fe98b16bf909d35e6d221fdcc4969869
Alireza Mohamadzadeh <alireza3205@gmail.com>
2025-08-19 00:53:48 +0330

    style: update LandingFooter styling and colors

--- src/app/components/LandingFooter/index.tsx
+++ src/app/components/LandingFooter/index.tsx
@@ -5,8 +5,8 @@ export default function LandingFooter() {
   const startYear = 2024;
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full mt-60 bg-[#1881fa] border-t border-base-300 shadow-inner text-white">
-      <div className="relative w-full h-32 bg-[#1881fa]">
+    <footer className="w-full mt-60 bg-primary">
+      <div className="relative bg-primary">
         <div id="wave1" className="wave" />
         <div id="wave2" className="wave" />
         <div id="wave3" className="wave" />
@@ -15,19 +15,19 @@ export default function LandingFooter() {
           direction="col"
           alignItems="center"
           justifyContent="center"
-          className="max-w-6xl mx-auto px-6 py-8 md:flex-row md:justify-between gap-6"
+          gap="L"
+          className="max-w-6xl mx-auto px-6 py-8 md:flex-row md:justify-between"
         >
           <div className="space-x-5">
             <GithubButton username="PhoenixTask" repo="WebApp" />
             <GithubButton username="PhoenixTask" repo="dotnet-backend" />
           </div>
 
-          <div className="w-52 text-xs">
+          <div className="w-52 text-xs text-white">
             © {startYear}-{currentYear} ساخته شده با 🩷
           </div>
         </Flex>
       </div>
-      <div className="bg-red-500 "></div>
     </footer>
   );
 }
